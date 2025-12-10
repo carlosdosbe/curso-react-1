87c1f8a1323f6658bdd4c02bdd8486a1e268524c
Carlos <carlosdosbe@gmail.com>
2025-12-09 19:13:45 -0600

    Avance hasta clase 32

---
+++ 1 - vite-project/src/bases/11-async-await.ts
@@ -0,0 +1,22 @@
+import type { GiphyRandomResponse, Gif } from "../data/giphy.response";
+
+const API_KEY = "EOkVlTm41v1dJn3aFYvY4GQyrNGHH6RH";
+
+const createImageInsideDOM = (url: string) => {
+  const imgElement = document.createElement("img");
+  imgElement.src = url;
+
+  document.body.append(imgElement);
+};
+
+const getImageUrl = async () => {
+  const myRequest = await fetch(
+    `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
+  );
+
+  const { data }: GiphyRandomResponse = await myRequest.json();
+
+  return data.images.original.url;
+};
+
+getImageUrl().then(createImageInsideDOM);
--- 1 - vite-project/src/main.ts
+++ 1 - vite-project/src/main.ts
@@ -9,6 +9,8 @@ import "./bases/06-obj-destructuring";
 import "./bases/07-array-destructuring";
 import "./bases/08-imp-exp";
 import "./bases/09-promises";
+import "./bases/10-fetch-api";
+import "./bases/11-async-await";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
